971f37c16184b777f569a2cf1bb9c02deb8d4f01
AKJ0407 <angkejin03@gmail.com>
2024-12-21 15:55:10 +0800

    made main loop code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #hahaha
 import json
 
-def openJson(filename) -> dict:
+def openJson(filename="test_user.json") -> dict:
     ## Open json file with file path of <filename>, return dictionary
     file = open(filename, 'r+')
     users = json.load(file)
@@ -15,8 +15,16 @@ def createUser(userID, users):
     return None
 
 def changePoints(newPoints, userID, users):
-    ## Add or subtract points form the user, return None
+    ## Add or subtract points for the user, return None
     users[userID] += newPoints
+    return None
+
+def changeQty(item, market, confirmation):
+    ## Add or subtract the item's quantity in the market, return None
+    if confirmation == 'Y':
+        market[item]["Qty"] += 1
+    else:
+        market[item]['Qty'] -= 1
     return None
 
 def commitJson(users, filename):
@@ -33,7 +41,7 @@ def commitJson(users, filename):
     file.close()
     return None
 
-def getPoints(item, filename) -> int:
+def getPoints(item, filename="test_market.json") -> int:
     ## Opens market dictionary, checks the appropriate points of the item, 
     # returns the points the item is worth
     ## Dictionary is in the format of { item : {"Qty": X, "Points": Y} }
@@ -49,4 +57,45 @@ def getPoints(item, filename) -> int:
 
 def main():
     ## Main loop
+    # Get user login, check if they exist in the system already
+    username = str(input("Enter your username: "))
+    users = openJson()
+    createUser(username, users)
+
+    # Format the market dictionary to display to the user
+    file = open("test_market.json", 'r+')
+    market = json.load(file)
+    file.close()
+    print('===========================================================')
+    print('     Material     |      Quantity      |      Points      |')
+    for material in market.keys():
+        print('     {}     |      {}      |      {}      |'.\
+              format(material, market[material]['Qty'], market[material['Points']]) )
+    
+    # Ask the user if they want to put in or remove, validate input
+    confirmation = str(input('Do you wish to put in items or take away items? (Y/N): ')).upper()
+    while confirmation != 'Y' and confirmation != 'N':
+        print('Invalid input!')
+        confirmation = str(input('Do you wish to put in items or take away items? (Y/N): ')).upper()
+    item = str(input('Please enter the item you wish to add or remove! (CASE-SENSITIVE): '))
+    while item not in market.keys():
+        print('Invalid input!')
+        item = str(input('Please enter the item you wish to add or remove! (CASE-SENSITIVE): '))
+    
+    # Get points of the item, update dictionaries accordingly
+    points = getPoints(item)
+    if confirmation == 'Y':
+        changePoints(points, username, users)
+    else:
+        changePoints(-points, username, users)
+    changeQty(item, market, confirmation)
+    print('Change applied! Enjoy your new item.')
+
+    # Display new Market dictionary to user.
+    print('===========================================================')
+    print('     Material     |      Quantity      |      Points      |')
+    for material in market.keys():
+        print('     {}     |      {}      |      {}      |'.\
+              format(material, market[material]['Qty'], market[material['Points']]) )   
+    print('Your current points is {}.'.format(users[username]))
     return None
