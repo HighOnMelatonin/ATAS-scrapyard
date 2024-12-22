55d7c4d142b57265c7f7b43d27b86ebfa977061d
AKJ0407 <angkejin03@gmail.com>
2024-12-22 12:06:05 +0800

    fixed naming conventions, created base.html

--- src/material_input.py
+++ src/material_input.py
@@ -6,17 +6,17 @@ app=Flask(__name__)
 def home():
     return render_template("index.html") #display index.html in templates folder
 
-@app.route("/login",methods=["POST","GET"])
-def login():
+@app.route("/material",methods=["POST","GET"])
+def material():
     if request.method=="POST":                      #if user submits data to nm
         user=request.form["material"]               #stores material variable into user
         return redirect(url_for("user",usr=user))   #redirects into user route
     else:
-        return render_template("login.html")
+        return render_template("material.html")
 
 @app.route("/<usr>")
 def user(usr):
-    return f"<h1>{"You entered {}. Have a nice day!".format(usr)}</h1>"
+    return f"<h1>{'You entered {}. Have a nice day!'.format(usr)}</h1>"
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
